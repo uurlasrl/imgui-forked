xqrc/imgui

RangeSelect/MultiSelect: Fix testing key mods from after the nav request (remove need to hold the mod longer)

imgui_internal.h
@@ -1164,6 +1164,7 @@ struct ImGuiContext
     bool                    MultiSelectEnabled;
     ImGuiMultiSelectFlags   MultiSelectFlags;
     ImGuiMultiSelectState   MultiSelectState;                   // We currently don't support recursing/stacking multi-select
+    ImGuiKeyModFlags        MultiSelectKeyMods;
 
     // Render
     ImDrawData              DrawData;                           // Main ImDrawData instance to pass render information to the user
@@ -1332,6 +1333,7 @@ struct ImGuiContext
 
         MultiSelectEnabled = false;
         MultiSelectFlags = ImGuiMultiSelectFlags_None;
+        MultiSelectKeyMods = ImGuiKeyModFlags_None;
 
         DimBgRatio = 0.0f;
         BackgroundDrawList._OwnerName = "##Background"; // Give it a name for debugging

imgui_widgets.cpp
@@ -5794,6 +5794,9 @@ ImGuiMultiSelectData* ImGui::BeginMultiSelect(ImGuiMultiSelectFlags flags, void*
     g.MultiSelectEnabled = true;
     g.MultiSelectFlags = flags;
 
+    // Use copy of keyboard mods at the time of the request, otherwise we would requires mods to be held for an extra frame.
+    g.MultiSelectKeyMods = g.NavJustMovedToId ? g.NavJustMovedToKeyMods : g.IO.KeyMods;
+
     if ((flags & ImGuiMultiSelectFlags_NoMultiSelect) == 0)
     {
         state->In.RangeSrc = state->Out.RangeSrc = range_ref;
@@ -5804,9 +5807,9 @@ ImGuiMultiSelectData* ImGui::BeginMultiSelect(ImGuiMultiSelectFlags flags, void*
     // FIXME: Polling key mods after the fact (frame following the move request) is incorrect, but latching it would requires non-trivial change in MultiSelectItemFooter()
     if (g.NavJustMovedToId != 0 && g.NavJustMovedToFocusScopeId == state->FocusScopeId && g.NavJustMovedToHasSelectionData)
     {
-        if (g.IO.KeyShift)
+        if (g.MultiSelectKeyMods & ImGuiKeyModFlags_Shift)
             state->InRequestSetRangeNav = true;
-        if (!g.IO.KeyCtrl && !g.IO.KeyShift)
+        if ((g.MultiSelectKeyMods & (ImGuiKeyModFlags_Ctrl | ImGuiKeyModFlags_Shift)) == 0)
             state->In.RequestClear = true;
     }
 
@@ -5890,13 +5893,13 @@ void ImGui::MultiSelectItemHeader(ImGuiID id, bool* p_selected)
     if (state->InRequestSetRangeNav)
     {
         IM_ASSERT(id != 0);
-        IM_ASSERT(g.IO.KeyShift);
+        IM_ASSERT((g.MultiSelectKeyMods & ImGuiKeyModFlags_Shift) != 0);
         const bool is_range_dst = !state->InRangeDstPassedBy && g.NavJustMovedToId == id;     // Assume that g.NavJustMovedToId is not clipped.
         if (is_range_dst)
             state->InRangeDstPassedBy = true;
         if (is_range_src || is_range_dst || state->In.RangeSrcPassedBy != state->InRangeDstPassedBy)
             selected = state->In.RangeValue;
-        else if (!g.IO.KeyCtrl)
+        else if ((g.MultiSelectKeyMods & ImGuiKeyModFlags_Ctrl) == 0)
             selected = false;
     }
 
@@ -5914,9 +5917,9 @@ void ImGui::MultiSelectItemFooter(ImGuiID id, bool* p_selected, bool* p_pressed)
 
     bool selected = *p_selected;
     bool pressed = *p_pressed;
-    bool is_ctrl = g.IO.KeyCtrl;
-    bool is_shift = g.IO.KeyShift;
     const bool is_multiselect = (g.MultiSelectFlags & ImGuiMultiSelectFlags_NoMultiSelect) == 0;
+    bool is_ctrl = (g.MultiSelectKeyMods & ImGuiKeyModFlags_Ctrl) != 0;
+    bool is_shift = (g.MultiSelectKeyMods & ImGuiKeyModFlags_Shift) != 0;
 
     // Auto-select as you navigate a list
     if (g.NavJustMovedToId == id)

uurlasrl/imgui-forked

imgui_internal.h
@@ -1903,6 +1903,7 @@ struct ImGuiContext
     bool                    MultiSelectEnabled;
     ImGuiMultiSelectFlags   MultiSelectFlags;
     ImGuiMultiSelectState   MultiSelectState;                   // We currently don't support recursing/stacking multi-select
+    ImGuiKeyChord           MultiSelectKeyMods;
 
     // Render
     float                   DimBgRatio;                         // 0.0..1.0 animation when fading in a dimming background (for modal window and CTRL+TAB list)
@@ -2120,6 +2121,7 @@ struct ImGuiContext
 
         MultiSelectEnabled = false;
         MultiSelectFlags = ImGuiMultiSelectFlags_None;
+        MultiSelectKeyMods = ImGuiMod_None;
 
         DimBgRatio = 0.0f;
         MouseCursor = ImGuiMouseCursor_Arrow;

imgui_widgets.cpp
@@ -6499,6 +6499,9 @@ ImGuiMultiSelectData* ImGui::BeginMultiSelect(ImGuiMultiSelectFlags flags, void*
     g.MultiSelectEnabled = true;
     g.MultiSelectFlags = flags;
 
+    // Use copy of keyboard mods at the time of the request, otherwise we would requires mods to be held for an extra frame.
+    g.MultiSelectKeyMods = g.NavJustMovedToId ? g.NavJustMovedToKeyMods : g.IO.KeyMods;
+
     if ((flags & ImGuiMultiSelectFlags_NoMultiSelect) == 0)
     {
         state->In.RangeSrc = state->Out.RangeSrc = range_ref;
@@ -6509,9 +6512,9 @@ ImGuiMultiSelectData* ImGui::BeginMultiSelect(ImGuiMultiSelectFlags flags, void*
     // FIXME: Polling key mods after the fact (frame following the move request) is incorrect, but latching it would requires non-trivial change in MultiSelectItemFooter()
     if (g.NavJustMovedToId != 0 && g.NavJustMovedToFocusScopeId == state->FocusScopeId && g.NavJustMovedToHasSelectionData)
     {
-        if (g.IO.KeyShift)
+        if (g.MultiSelectKeyMods & ImGuiMod_Shift)
             state->InRequestSetRangeNav = true;
-        if (!g.IO.KeyCtrl && !g.IO.KeyShift)
+        if ((g.MultiSelectKeyMods & (ImGuiMod_Ctrl | ImGuiMod_Shift)) == 0)
             state->In.RequestClear = true;
     }
 
@@ -6591,13 +6594,13 @@ void ImGui::MultiSelectItemHeader(ImGuiID id, bool* p_selected)
     if (state->InRequestSetRangeNav)
     {
         IM_ASSERT(id != 0);
-        IM_ASSERT(g.IO.KeyShift);
+        IM_ASSERT((g.MultiSelectKeyMods & ImGuiMod_Shift) != 0);
         const bool is_range_dst = !state->InRangeDstPassedBy && g.NavJustMovedToId == id;     // Assume that g.NavJustMovedToId is not clipped.
         if (is_range_dst)
             state->InRangeDstPassedBy = true;
         if (is_range_src || is_range_dst || state->In.RangeSrcPassedBy != state->InRangeDstPassedBy)
             selected = state->In.RangeValue;
-        else if (!g.IO.KeyCtrl)
+        else if ((g.MultiSelectKeyMods & ImGuiMod_Ctrl) == 0)
             selected = false;
     }
 
@@ -6615,9 +6618,9 @@ void ImGui::MultiSelectItemFooter(ImGuiID id, bool* p_selected, bool* p_pressed)
 
     bool selected = *p_selected;
     bool pressed = *p_pressed;
-    bool is_ctrl = g.IO.KeyCtrl;
-    bool is_shift = g.IO.KeyShift;
     const bool is_multiselect = (g.MultiSelectFlags & ImGuiMultiSelectFlags_NoMultiSelect) == 0;
+    bool is_ctrl = (g.MultiSelectKeyMods & ImGuiMod_Ctrl) != 0;
+    bool is_shift = (g.MultiSelectKeyMods & ImGuiMod_Shift) != 0;
 
     // Auto-select as you navigate a list
     if (g.NavJustMovedToId == id)